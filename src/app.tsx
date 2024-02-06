a45ff76cd329810f5bc384971439d54011086682
Carlos André <carloshlz.andre@gmail.com>
2024-02-06 12:36:50 -0300

    Propriedades, eventos, estados, modais e formulários

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+
 export function App() {
  return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -21,10 +22,11 @@ export function App() {
             
             <NewNoteCard/>
 
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
-            <NoteCard/>
+            <NoteCard note={{
+                date: new Date(),
+                 content: 'Hello World'
+            }}/>
+            
             
 
 
